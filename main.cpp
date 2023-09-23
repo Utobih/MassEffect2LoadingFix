aed5518f3b7371c1ea2ec4fa30aa2b4d7afccee7
Robert <48176914+zahanzo@users.noreply.github.com>
2023-09-22 21:25:46 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
+// CREATED BY: 0xRobert, 2021, github.com/zahanzo
+
 #include <iostream>
 #include <fstream>
-#include "include/regv.h"
+#include <windows.h>
+#include <algorithm>
 #include "include/resource.h"
 
 using namespace std;
+
+// REG READ
+std::string RegRead (HKEY hkey, LPCTSTR subkey,LPCTSTR name,DWORD type){
+	HKEY key;
+	TCHAR value[255];
+	DWORD value_length = 255;
+	RegOpenKey(hkey,subkey,&key);
+	RegQueryValueEx(key,name, NULL, &type, (LPBYTE)&value, &value_length);
+	RegCloseKey(key);
+	std::string final_value = value;
+	std::replace( final_value.begin(), final_value.end(), '\\', '/');
+	return final_value;
+}
 
 int starting(string game_location){
     system("Color B5");
@@ -13,12 +29,10 @@ int starting(string game_location){
     string disk_loc = game_location.substr(0, 2);
     //creating cmd var with commands
     string l_cmd = (string(" && start /affinity 2 MassEffect2.exe"));
-    //creating final command var
     string cmd_f = (disk_loc + string(" && cd \"")+binarie_location+string("\"")+l_cmd);
 
     //executing cmd
     system(cmd_f.c_str());
-    //exiting
     cout << "[+] Done! starting game with loading boost." << endl;
     system("pause");
     return 0;
@@ -39,7 +53,7 @@ int main(){
 
     } else { //if dont
         cout << "[*] Config file doesn't exist..\n[*] Getting game path from windows reg.." << endl;
-        //Getting game location from reg and adding to var
+        //Getting game location from reg
         string game_location = RegRead(HKEY_LOCAL_MACHINE, "Software\\BioWare\\Mass Effect 2", "Path", REG_SZ);
         starting(game_location);
     }
